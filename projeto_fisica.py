6775ac761d72bfe258b16081d0faa9023b8a97bb
Jota <nullifidianz@gmail.com>
2023-08-30 00:14:16 -0300

    criaçao do arquivo python do Projeto de Física sobre Ondas Eletromagnéticas

---
+++ projeto_fisica.py
@@ -0,0 +1,15 @@
+import math
+
+def info():
+    print("Programa para o estudo das ondas eletromagnéticas")
+    print("Este programa procura realziar conversões entre diferentes unidades e calcular valores como:\nfrequência\ncomprimento de onda\nnúmero de onda\nfrequência angular\namplitude do campo elétrico\namplitude do campo magnético\nintensidade")
+    print()
+    print("\tFeito por: João Paulo Paggi Zuanon Dias - RA: 22.222.058-4")
+    print("\tFeito por: Matheus - RA: xx.xxx.xxx-x")
+    print("\tFeito por: Leandro - RA: xx.xxx.xxx-x")
+
+def main():
+    info()
+
+if __name__ == "__main__":
+    main()
