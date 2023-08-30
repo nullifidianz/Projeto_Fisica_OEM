f9a955404473a9ef6feef37d9865719a63270bb4
Jota <nullifidianz@gmail.com>
2023-08-30 00:32:00 -0300

    Implementação de um menu de escolha para o direcionamento dos calculos

--- projeto_fisica.py
+++ projeto_fisica.py
@@ -4,12 +4,21 @@ def info():
     print("Programa para o estudo das ondas eletromagnéticas")
     print("Este programa procura realziar conversões entre diferentes unidades e calcular valores como:\nfrequência\ncomprimento de onda\nnúmero de onda\nfrequência angular\namplitude do campo elétrico\namplitude do campo magnético\nintensidade")
     print()
-    print("\tFeito por: João Paulo Paggi Zuanon Dias - RA: 22.222.058-4")
-    print("\tFeito por: Matheus - RA: xx.xxx.xxx-x")
-    print("\tFeito por: Leandro - RA: xx.xxx.xxx-x")
+    print("Feito por: João Paulo Paggi Zuanon Dias - RA: 22.222.058-4")
+    print("Feito por: Matheus - RA: xx.xxx.xxx-x")
+    print("Feito por: Leandro - RA: xx.xxx.xxx-x")
+    print()
+    print("***************************************************************",)
+
+def menu():
+    print("Escolha uma opção:")
+    print("\t1. Calcular valores a partir de Em, Bm ou I")
+    print("\t2. Calcular valores a partir de f, λ, k ou ω")
+    print("\t3. Sair")
 
 def main():
     info()
+    menu()
 
 if __name__ == "__main__":
     main()
