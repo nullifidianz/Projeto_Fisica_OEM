16bf45771594f021b8f5eb39b56d9fc7802ecb0f
Jota <nullifidianz@gmail.com>
2023-08-30 23:40:17 -0300

    Modificação nas variáveis e teste da função de calculo 2

--- projeto_fisica.py
+++ projeto_fisica.py
@@ -17,7 +17,7 @@ def info():
 def menu():
     print("Escolha uma opção:")
     print("\t1. Calcular valores a partir de Em, Bm ou I")
-    print("\t2. Calcular valores a partir de f, λ, k ou ω")
+    print("\t2. Calcular valores a partir de f, λ (lambda), k ou ω")
     print("\t3. Sair")
 
 def calculos1():
@@ -25,19 +25,19 @@ def calculos1():
     x = input("Escolha a grandeza Em / Bm / I: ")
 
     if x=="Em":
-        Em = float(input("informe a amplitude do Campo Elétrico (Em) em V/m"))
+        Em = float(input("informe a amplitude do Campo Elétrico (Em) em V/m: "))
         Bm = (Em/c)
         I = (0.5*Em*Bm)
         print(f"A amplitude do Campo Magnético (Bm): {Bm:.3e} T")
         print(f"Intensidade (I): {I:.3e} W/m^2")
     elif x=="Bm":
-        Bm = float(input("Informe a amplitude do Campo Magnético (Bm) em V/m"))
+        Bm = float(input("Informe a amplitude do Campo Magnético (Bm) em V/m: "))
         Em = (Bm*c)
         I = (0.5*Em*Bm)
         print(f"A amplitude do Campo Elétrico (Em): {Em:.3e} T")
         print(f"Intensidade (I): {I:.3e} W/m^2")
     elif x=="i":
-        I = float(input("Informe a intensidade (I) em W/m^2"))
+        I = float(input("Informe a intensidade (I) em W/m^2: "))
         Em = math.sqrt(2*I)
         Bm = (Em/c)
         print(f"A amplitude do Campo Elétrico (Em): {Em:.3e} T")
@@ -46,7 +46,24 @@ def calculos1():
         print("Opção Inválida.")
 
 def calculos2():
-    print("teste calc2")
+    print("Calculos a partir de f, lambda, k ou ω (Frequência Angular)")
+    y = input("Escolha a grandeza a partir de f, λ (lambda), k ou ω (Frequência Angular)")
+
+    if y=="f":
+        print("Teste f")
+    
+    elif y=="lambda" or y=="λ":
+        print("teste lambda")
+    
+    elif y=="k":
+        print("teste k")
+    
+    elif y=="ω" or y=="Freqência Angular":
+        print("teste Frequência Angular")
+    else:
+        print("Opção Inválida.")
+
+
 
 def main():
     info()
